39ab1a109657111662f4f64145c10654f0979934
Leanne <ltcre8lab@outlook.com>
2024-09-14 11:21:40 +1000

    indentation and pycodestyle error fixed

--- adventure_game.py
+++ adventure_game.py
@@ -3,15 +3,19 @@ import random
 weapons = random.choice(['axe', 'bow', 'sword', 'dagger'])
 enemy = random.choice(['dracula', 'skeletons', 'wicked fairie', 'dragon'])
 
+
 def print_pause(message_to_print):
     print(message_to_print)
     time.sleep(2)
 
+
 def intro():
-    print_pause("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
-    print_pause(f"Rumor has it that a {enemy} is somewhere around here," 
+    print_pause("You find yourself standing in an open field,"
+                "filled with grass and yellow wildflowers.")
+    print_pause(f"Rumor has it that a {enemy} is somewhere around here,"
                 "and has been terrifying the nearby village.")
     field()
+
 
 def field():
     print_pause("In front of you is a house.")
@@ -27,6 +31,7 @@ def field():
     else:
         print_pause("Please enter 1 or 2\n")
 
+
 def house():
     print_pause("You are about to knock on the door.")
     print_pause("You walk inside the house.")
@@ -34,18 +39,19 @@ def house():
     print_pause(f"Yikes!, This is the {enemy} house!")
     fight()
 
+
 def fight():
     print_pause(f"The {enemy} start to attacks you!")
     fight = input("Would you like to (1) fight or (2) run away?\n")
     if fight == '1':
         print_pause(f"The {enemy} moves towards you to attack")
         print_pause(f"You grab new {weapons}")
-        print_pause(f"The shiny {weapons} of Wudang in your hand as you make an attack.")
+        print_pause(f"The shiny {weapons} of Wudang in your"
+                    "hand as you make an attack.")
         print_pause(f"Attack! {enemy}")
         print_pause(f"The {enemy} is now destroyed.")
         print_pause(f"Victory!, You have defeated the {enemy}.")
         play_again()
-
     elif fight == '2':
         print_pause("You run back to the open field.")
         print_pause("Luckily, no enemies follow you.")
@@ -57,6 +63,7 @@ def fight():
         print_pause("Game Over!")
         play_again()
 
+
 def cave():
     print_pause("This is an empty cave.")
     print_pause("You have been to this place before,"
@@ -64,6 +71,7 @@ def cave():
     print_pause(f"You have found magical {weapons} located inside the crate.")
     print_pause("Walk back out to the field.")
     field()
+
 
 def play_again():
     choice = input("Would you like to play again? (y or n)").lower()
@@ -75,8 +83,10 @@ def play_again():
     else:
         print_pause("Please enter y or n.")
 
+
 # Starts the game function
 def play_game():
     intro()
+
 
 play_game()
